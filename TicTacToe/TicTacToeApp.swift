194344ca620a025580f52c00856f8160df0b1838
mallavarapuanoop <mallavarapuanoop.com>
2022-05-29 20:29:33 +0530

    First Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Anoop Mallavarapu on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
